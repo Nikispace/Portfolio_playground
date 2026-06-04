fd2b667d61128ea4bcde5e29193dbee2692c325b
nikispace <nikithanagami@gmail.com>
2026-06-04 18:27:07 +0530

    Fix Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using relative paths so it works seamlessly on GitHub Pages
+  base: '/Portfolio_playground/',
 })
